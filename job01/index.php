1ec41c1cb4562fb1c0a6167c3194d24f8066b053
alex-tornier <alex.tornier@laplateforme.io>
2022-11-21 12:14:05 +0100

    first

---
+++ job01/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    $str = 'vive';
+
+    $str2 = 'laplateforme';
+
+    $str3 = '!';
+    
+    echo "$str1 $str2 $str3<br/>";
+
+    $val = 6;
+    echo "$val<br/>";
+    echo $val+4;
+    echo "<br/>";
+
+    $myBool = true;
+    echo "$myBool<br/>";
+
+    $myBool = false;
+    echo "$myBool<br/>";
+?>
